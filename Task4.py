3f2991aca37a2ca7e53e91ddbed5e586a4298b5d
Felix Kiang <jiangxiaofeng@live.com>
2018-02-16 17:36:03 +0800

    Add files via upload

--- Task4.py
+++ Task4.py
@@ -1,12 +1,4 @@
 """
-Intro to Python Lab 1, Task 4
-
-Complete each task in the file for that task. Submit the whole folder
-as a zip file or GitHub repo. 
-Full submission instructions are available on the Lab Preparation page.
-"""
-
-"""
 Read file into texts and calls. 
 It's ok if you don't understand how to read files
 You will learn more about reading files in future lesson
@@ -20,6 +12,40 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+
+#step1:find no-repeated call when make and receive.
+"""
+calls_no_repeat_1:make no-repeated outcalling calls
+calls_no_repeat_2:received no -repeated calls which has two characteris:
+never send texts,receive texts 
+"""
+calls_no_repeat_1 = []
+calls_no_repeat_2 = []
+
+for call in calls:
+	if call[0] not in calls_no_repeat_1:
+		calls_no_repeat_1.append(call[0])
+	if call[1] not in calls_no_repeat_2:
+		calls_no_repeat_2.append(call[1])
+
+for text in texts:#add telephone# which send and receive texts
+	if text[0] not in calls_no_repeat_2:
+		calls_no_repeat_2.append(text[0])
+	if text[1] not in calls_no_repeat_2:
+		calls_no_repeat_2.append(text[1])
+
+#step2:find the 
+telemarketers = list(set(calls_no_repeat_1).difference(set(calls_no_repeat_2)))
+telemarketers.sort()
+
+print("These numbers could be telemarketers:")
+for i in telemarketers:
+	print('\n'+i)
+
+
+
+
+
 
 """
 TASK 4:
